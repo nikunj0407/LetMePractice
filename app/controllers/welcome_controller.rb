f8631cab5e361547eeac6ce32e03a097064f6561
nikunj <nikunj@Shrihari.(none)>
2014-02-08 22:42:30 +0530

    MCQ1 Numbering issue resolved

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,8 +17,7 @@ class WelcomeController < ApplicationController
       if !@previous_question_set.blank?
         #@fill_in_the_blank_chapter = Question.find_by_sql("select * from (SELECT *, row_number() over (partition by chapter_id order by random()) FROM questions LEFT OUTER JOIN test_details on test_details.question_id != questions.id where questions.type = 'FillInTheBlank') a where row_number=1;")
         @fill_in_the_blank = Question.find_by_sql("select * from (SELECT *, row_number() over (partition by chapter_id order by random()) FROM questions where questions.type = 'FillInTheBlank' AND questions.id NOT IN (" + @previous_question_set.map { |i| i.question_id }.to_s.gsub('[', '').gsub(']', '') + ")) a where row_number=1;").sample(6)
-        #render json: @fill_in_the_blank
-        #return
+
         @fill_in_the_blank_next = Question.where(type: 'FillInTheBlank').where(['id NOT IN (?)', @fill_in_the_blank.map { |q| q.id }]).order('RANDOM()').limit(4).sample(4)
 
         @true_false = Question.find_by_sql("select * from (SELECT *, row_number() over (partition by chapter_id order by random()) FROM questions where questions.type = 'TrueFalse' AND questions.id NOT IN (" + @previous_question_set.map { |i| i.question_id }.to_s.gsub('[', '').gsub(']', '') + ")) a where row_number=1;").sample(6)
@@ -46,7 +45,7 @@ class WelcomeController < ApplicationController
         @true_false_next = Question.where(type: 'TrueFalse').where(['id NOT IN (?)', @true_false.map { |q| q.id }]).order('RANDOM()').limit(4).sample(4)
 
         @mcq1 = Question.find_by_sql("select * from (SELECT *, row_number() over (partition by chapter_id order by random()) FROM questions where questions.type = 'Mcq1') a where row_number between 1 AND 2;").sample(12)
-        @mcq1_next = Question.where(type: 'Mcq1').where(['id NOT IN (?)', @mcq1.map { |q| q.id }]).order('RANDOM()').limit(2).sample(8)
+        @mcq1_next = Question.where(type: 'Mcq1').where(['id NOT IN (?)', @mcq1.map { |q| q.id }]).order('RANDOM()').limit(8).sample(8)
 
         @mcq2 = Question.where(type: 'Mcq2').order('RANDOM()').limit(5)
 
@@ -68,7 +67,7 @@ class WelcomeController < ApplicationController
       @true_false_next = Question.where(type: 'TrueFalse').where(['id NOT IN (?)', @true_false.map { |q| q.id }]).limit(4).sample(4)
 
       @mcq1 = Question.find_by_sql("select * from (SELECT *, row_number() over (partition by chapter_id) FROM questions where questions.type = 'Mcq1') a where row_number between 1 AND 2;").sample(12)
-      @mcq1_next = Question.where(type: 'Mcq1').where(['id NOT IN (?)', @mcq1.map { |q| q.id }]).limit(2).sample(8)
+      @mcq1_next = Question.where(type: 'Mcq1').where(['id NOT IN (?)', @mcq1.map { |q| q.id }]).limit(8).sample(8)
 
       @mcq2 = Question.where(type: 'Mcq2').limit(5)
 
